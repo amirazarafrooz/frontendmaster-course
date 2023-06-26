89f0795c61284b3af1dbd11c35af2b3a7e4852a7
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-06-26 13:16:16 +0330

    other stuff section

---
+++ other-stuff/script.js
@@ -0,0 +1,14 @@
+const ball = document.querySelector('.ball')
+console.log('ball');
+
+popmotion.animate({
+    from: "0px",
+    to: "100px",
+    repeat: Infinity,
+    repeatType: "mirror",
+    type: "spring",
+    onUpdate(update) {
+        console.log(update);
+        ball.style.left = update;
+    }
+});
